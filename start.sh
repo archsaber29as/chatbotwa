74d343b784acfce4f2f255ff74b9da1faa988bf8
Devan Ramadhana <devan.ramadhana@mastersystem.co.id>
2026-04-21 22:30:24 +0700

    new sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-   echo "$GOOGLE_CREDENTIALS_B64" | base64 -d > credentials.json
-   echo "$GOOGLE_TOKEN_B64" | base64 -d > token.pickle
-   python app.py
+echo "$GOOGLE_CREDENTIALS_B64" | base64 -d > credentials.json
+echo "$GOOGLE_TOKEN_B64" | base64 -d > token.pickle
+python app.py
